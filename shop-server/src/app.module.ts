d5ba1d9ab45b2b218ab4f9f8b758b3d5234883cb
ilik-sh <i.shyshparonak@gmail.com>
2023-12-14 12:58:52 +0300

    Add ConfigModule configuration

--- shop-server/src/app.module.ts
+++ shop-server/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './app/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import securityConfig from 'config/security.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [securityConfig],
+      isGlobal: true
+    }),
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
