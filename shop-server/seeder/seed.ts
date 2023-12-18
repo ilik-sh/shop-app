3641a8ace913ce620dbe615a68e443749a05a59d
ilik-sh <i.shyshparonak@gmail.com>
2023-12-18 22:17:03 +0300

    Create seeder

---
+++ shop-server/seeder/seed.ts
@@ -0,0 +1,36 @@
+import { faker } from "@faker-js/faker";
+import { PrismaClient, Product } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function createProducts(quantity: number) {
+    for (let i = 0; i < quantity; i++) {
+        const productName = faker.commerce.productName()
+
+        const product = await prisma.product.create({
+            data: {
+                name: productName,
+                slug: faker.helpers.slugify(productName).toLowerCase(),
+                description: faker.commerce.productDescription(),
+                price: +faker.commerce.price({min: 1, max: 999, dec: 0}),
+                image: faker.image.url(),
+                stock: faker.number.int({min: 1, max: 100}),
+                category: {
+                    create: {
+                        name: faker.commerce.department()
+                    }
+                }
+            }
+        })
+    }
+}
+
+async function main () {
+    await createProducts(10)
+}
+
+main()
+    .catch(e => console.log(e))
+    .finally(async () => {
+        prisma.$disconnect
+    })
