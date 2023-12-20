122f8afe84d3baca0238a387d20875d790302ec0
ilik-sh <i.shyshparonak@gmail.com>
2023-12-20 15:52:22 +0300

    Add additional error codes

--- shop-server/src/enums/errorCode.enum.ts
+++ shop-server/src/enums/errorCode.enum.ts
@@ -4,5 +4,10 @@ export enum ErrorCodes {
     InvalidForm = 'Invalid form',
     UserCreationFailed = 'User creation failed',
     InvalidRefreshToken = 'Invalid refresh token',
-    InvalidRequest = 'Invalid request'
+    InvalidRequest = 'Invalid request',
+    InsufficientProduct = 'Insufficient Product',
+    OrderCreationFailed = 'Order Creation Failed',
+    OrderItemDoesNotExist = 'Order Item Does Not Exist',
+    OrderDoesNotExist = 'Order Does Not Exist',
+    ProductDoesNotExist = 'Product Does Not Exist',
 }
