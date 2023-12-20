ee410fcdb1f593dd73d1b60813f3066d05cbf63f
ilik-sh <i.shyshparonak@gmail.com>
2023-12-20 15:57:37 +0300

    Fix decorator not getting valid data from request

--- shop-server/src/decorators/currentUser.decorator.ts
+++ shop-server/src/decorators/currentUser.decorator.ts
@@ -4,6 +4,6 @@ import { UserSessionDto } from "domain/dtos/userSession.dto";
 export const CurrentUser = createParamDecorator(
     (data: unknown, context: ExecutionContext) => {
       const request = context.switchToHttp().getRequest();
-      return request as UserSessionDto;
+      return request.user;
     },
-  );
+  );  
